048b965a81f33346e23fc82e90b58cdd1b744f8c
huangting <350358955@qq.com>
2013-07-08 17:35:27 +0800

    test comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-
+# get the current cart
     def current_cart 
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
